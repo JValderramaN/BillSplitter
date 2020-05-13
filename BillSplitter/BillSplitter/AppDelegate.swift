1cc4f153f0a8138dd12ab7f8f6764cb486bd0fd6
José Valderrama <josevalderrama18@gmail.com>
2020-05-13 13:36:22 -0300

    * Setup xcode project

---
+++ BillSplitter/BillSplitter/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BillSplitter
+//
+//  Created by José Valderrama on 13/05/2020.
+//  Copyright © 2020 José Valderrama. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+    
+}
+
---
+++ BillSplitter/BillSplitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BillSplitter
+//
+//  Created by José Valderrama on 13/05/2020.
+//  Copyright © 2020 José Valderrama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
